5f5936380d0efe514fcdf4dc3c420d698eb7b552
Carlo Felix Zanotta Dumit <ra095635@felix.lab.ic.unicamp.br>
2014-03-26 17:01:26 -0300

    DONE

--- npData.h
+++ npData.h
@@ -34,39 +34,3 @@ typedef struct _newspaper
 	newsItemList *newsList;
 
 } newspaper;
-/*
-
-newsItem* duplicateNewsItem(newsItem* oldItem){
-	newsItem *item = (newsItem*) malloc(sizeof(newsItem));
-	item->title = strdup(oldItem->title);
-	item->abstract = strdup(oldItem->abstract);
-	item->author = strdup(oldItem->author);
-	item->date = strdup(oldItem->date);
-	item->text = strdup(oldItem->text);
-	item->text = strdup(oldItem->text);
-	item->source = strdup(oldItem->source);
-	item->image = strdup(oldItem->image);
-
-	if(oldItem->structure == NULL) item->structure = NULL;
-	
-}
-
-typedef struct np{
-	char *title;
-	char *date;
-	structure *structure;
-
-
-	
-} t_list;  
-
-struct list_name {
-	char* name;
-	struct list_name *next;
-};
-
-
-t_list *head_list;
-t_list *tail_list;
-
-*/
